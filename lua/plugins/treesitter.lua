153a6392a07051af0268b12731bc9d4fadd3ff60
Artin <artinkordzangeneh@gmail.com>
2024-09-20 14:32:57 +0330

    Update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         require("nvim-treesitter.configs").setup {
             auto_install = true,
-            ensure_installed = { "lua", "vim", "rust", "css", "html", "javascript", "python", "julia", "c", "cpp", "c_sharp" },
+            ensure_installed = { "lua", "vim", "rust", "css", "html", "javascript", "python", "julia", "c", "cpp", "c_sharp", "go", "java", "zig" },
             highlight = { enable = true },
             indent = { enable = true },
         }
